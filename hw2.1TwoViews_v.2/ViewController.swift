cdbbf2bdbb7c682be1eda6deba30fc94dbc5857a
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-08 16:43:11 +0300

    Initial Commit

---
+++ hw2.1TwoViews_v.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2.1TwoViews_v.2
+//
+//  Created by Tato Admin on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
